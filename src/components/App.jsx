ddc02a51c3f2c7d8c1933bb4df2d1ce886e95966
Arthur Revelski <arthurrevelski@Arthur-Revelskis-iMac.local>
2021-04-06 19:22:52 -0600

    set up initial results sort

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,11 +12,25 @@ const App = () => {
         console.log(sortParams);
     }, [sortParams]);
 
-    const submitHandler = async (e) => {
+    const apiURL = 'https://api.github.com/search/repositories?per_page=30&';
+
+    const searchSubmitHandler = async (e) => {
         e.preventDefault();
         try {
-            const getApi = await fetch(`https://api.github.com/search/repositories?q=${searchParams.replace(' ', '+')}&per_page=100&sort=${sortParams}`);
+            const getApi = await fetch(`${apiURL}q=${searchParams.replace(' ', '+')}`);
             if (!getApi.ok) throw new Error('Github Request failed');
+            const apiResponse = await getApi.json();
+            setSearchResults(apiResponse);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const filterSubmitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const getApi = await fetch(`${apiURL}q=${searchParams.replace(' ', '+')}&sort=${sortParams}`);
+            if (!getApi.ok) throw new Error('Github Filter Request failed');
             const apiResponse = await getApi.json();
             setSearchResults(apiResponse);
         } catch (err) {
@@ -26,10 +40,10 @@ const App = () => {
 
     const payload = searchResults ? searchResults.items.map((e) => {
         const {
-            name, stargazers_count, language, description,
+            name, stargazers_count, language, description, node_id,
         } = e;
         return (
-            <div className="card text-dark bg-light mb-4">
+            <div className="card text-dark bg-light mb-4" key={node_id}>
                 <h4 className="card-header">{name}</h4>
                 <div className="card-body">
                     <p><b>{description}</b></p>
@@ -41,7 +55,7 @@ const App = () => {
                         <Icon icon="LANGUAGE" fill="#0055b0" />
                         {`Language: ${language} `}
                     </p>
-                    <a href="/" className="btn btn-primary btn-sm">Show Details</a>
+                    <a href="/" className="btn btn-secondary btn-sm">Show Details</a>
                 </div>
             </div>
         );
@@ -51,7 +65,7 @@ const App = () => {
         <div className="container mt-4">
             <h1 className="fs-5">Github Repository Search</h1>
             <section>
-                <form onSubmit={submitHandler} className="mb-4">
+                <form onSubmit={searchSubmitHandler} className="mb-4">
                     <label htmlFor="github-search" className="col-form-label-sm">Search Github Repositories</label>
                     <input
                         type="text"
@@ -60,23 +74,24 @@ const App = () => {
                         onChange={(e) => setSearchParams(e.target.value)}
                     />
                     <button type="submit" className="btn btn-primary">Search</button>
-                    {
-                        payload && (
-                            <>
-                                <hr className="mt-4" />
-                                <label htmlFor="github-search" className="col-form-label-sm">Sort Results By</label>
-                                <select
-                                    className="form-control mb-4 col-12 col-md-4"
-                                    id="sort-by"
-                                    onChange={(e) => setSortParams(e.target.value)}
-                                >
-                                    <option value="best_match">Best Match</option>
-                                    <option value="stars">Most Stars</option>
-                                </select>
-                            </>
-                        )
-                    }
                 </form>
+                {
+                    payload && (
+                        <form onSubmit={filterSubmitHandler} className="mb-4">
+                            <label htmlFor="sort-by" className="col-form-label-sm">Sort Results By</label>
+                            <select
+                                className="form-control mb-4 col-12 col-md-4"
+                                id="sort-by"
+                                onChange={(e) => setSortParams(e.target.value)}
+                            >
+                                <option value="best_match">Best Match</option>
+                                <option value="stars">Most Stars</option>
+                                <option value="fewest-stars">Fewest Stars</option>
+                            </select>
+                            <button type="submit" className="btn btn-primary">Refine Search</button>
+                        </form>
+                    )
+                }
             </section>
             <section>
                 {payload}
