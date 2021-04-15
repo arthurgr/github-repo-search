318c0531af4bbd84bba44e7eae7f5150f8de4143
Arthur Revelski <arthurrevelski@Arthur-Revelskis-iMac.local>
2021-04-14 23:32:23 -0600

    updated dev server for refresh

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,6 +8,7 @@ module.exports = merge(common, {
     devtool: 'inline-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, './dist'),
+        historyApiFallback: true,
         hot: true,
     },
     plugins: [
